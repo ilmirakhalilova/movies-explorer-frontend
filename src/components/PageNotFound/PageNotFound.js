7d6b0ca2888af26e27f1674a53cec32ca466b8b0
ilmira khalilova <mirakhaleesi@yandex.ru>
2023-11-19 21:53:45 +0300

    на первое ревью 4.2 этапа

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,12 +1,14 @@
 import './PageNotFound.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
   return (
     <main className="page-not-found">
       <h1 className="page-not-found__title">404</h1>
       <h2 className="page-not-found__subtitle">Страница не найдена</h2>
-      <Link to="/" className="page-not-found__link animation">Назад</Link>
+      <button className="page-not-found__link animation" type="button" onClick={() => navigate(-1)}>Назад</button>
     </main>
   )
 }
